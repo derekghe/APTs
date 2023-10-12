e718da18077d4dca951f04967684c280fb3188b1
Derek He <32006904+derekghe@users.noreply.github.com>
2023-10-12 16:05:47 -0400

    updated alphalength

--- src/AlphaLength.java
+++ src/AlphaLength.java
@@ -2,22 +2,15 @@ import java.util.*;
 
 public class AlphaLength {
     public ListNode create (String[] words) {
-        ArrayList<String> al = new ArrayList<>(Arrays.asList(words));
-        Collections.sort(al);
-        HashSet<String> set = new LinkedHashSet<>(al);
-        
+        TreeSet<String> ts = new TreeSet<>(Arrays.asList(words));
+        ArrayList<String> list = new ArrayList<>(ts);
 
-        ListNode first = null;
-        ListNode current = null;
+        ListNode first = new ListNode(list.get(0).length(), null);
+        ListNode current = first;
 
-        for(String word: set) {
-            if(first == null) {
-                first = new ListNode(word.length(), null);
-                current = first;
-            } else {
-                current.next = new ListNode(word.length(), null);
-                current = current.next;
-            }
+        for(int i=1; i<list.size(); i++){
+            current.next = new ListNode(list.get(i).length(), null);
+            current = current.next;
         }
 
         return first;
