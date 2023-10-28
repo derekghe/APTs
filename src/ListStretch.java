358be88e7eb5fda53b55dbccfa30c1865f67c6b2
Derek He <32006904+derekghe@users.noreply.github.com>
2023-10-27 23:31:22 -0400

    updated ListStretch

--- src/ListStretch.java
+++ src/ListStretch.java
@@ -1,20 +1,16 @@
 public class ListStretch {
     public ListNode stretch (ListNode list, int amount){
-        if(list == null) return null;
-
-        ListNode first = new ListNode(list.info);
-        ListNode current = first;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
 
         while(list != null){
-            for(int i = 0; i < amount; i++){
+            for(int i=0; i<amount; i++){
                 current.next = new ListNode(list.info);
                 current = current.next;
             }
             list = list.next;
         }
 
-        first = first.next;
-
-        return first;
+        return dummy.next;
     }
 }
