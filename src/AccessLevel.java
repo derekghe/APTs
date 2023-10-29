5dc293948cb34f153adff5a0cf3b8f90712bf279
Derek He <32006904+derekghe@users.noreply.github.com>
2023-10-28 23:10:20 -0400

    Update AccessLevel.java

--- src/AccessLevel.java
+++ src/AccessLevel.java
@@ -1,6 +1,7 @@
 public class AccessLevel {
     String result = "";
      public String canAccess(int[] rights, int minPermission) {
+         
             for(int i = 0; i < rights.length; i++){
                 if(rights[i] >= minPermission){
                     result = result + "A";
@@ -9,6 +10,7 @@ public class AccessLevel {
                     result = result + "D";
                 }
             }
+         
             return result;
      }
   }
