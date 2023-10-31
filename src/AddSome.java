de2d41b3e7414cc4ea8156e6e595dca2cac12ae3
Derek He <32006904+derekghe@users.noreply.github.com>
2023-10-30 22:19:37 -0400

    Update AddSome.java

--- src/AddSome.java
+++ src/AddSome.java
@@ -1,19 +1,19 @@
 public class AddSome {
     public ListNode splice(ListNode list, int size){
-        
+
         ListNode remember = list;
-        ListNode first = new ListNode(list.info);
-        ListNode last = first;
-        list = list.next;
-        
-        for(int k=0; k < size-1; k++){
-            last.next = new ListNode(list.info);
-            last = last.next;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        for(int i = 0; i < size; i++){
+            current.next = new ListNode(list.info);
             list = list.next;
+            current = current.next;
         }
-        
-        last.next = remember;
-        
-        return first;
+
+        current.next = remember;
+
+        return dummy.next;
+
     }
-  }
+}
