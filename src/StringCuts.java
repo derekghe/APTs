85848603af025a7adff2f74f31bd1483268f207b
Derek He <32006904+derekghe@users.noreply.github.com>
2023-10-31 11:31:00 -0400

    Update StringCuts.java

--- src/StringCuts.java
+++ src/StringCuts.java
@@ -1,11 +1,15 @@
 import java.util.*;
+
 public class StringCuts {
      public String[] filter(String[] list, int minLength) {
-        HashSet <String> a = new LinkedHashSet<>();
+          
+        HashSet<String> a = new LinkedHashSet<>();
+
         for (String word: list){
             if (word.length() >= minLength)
                 a.add(word);
         }
-        return a.toArray(new String[a.size()]);
+        
+        return a.toArray(new String[0]);
      }
  }
