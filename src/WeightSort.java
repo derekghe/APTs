fbb18f2d0e00a7bf9d7fb4eddcdb7abb9785df67
Derek He <32006904+derekghe@users.noreply.github.com>
2023-12-08 18:32:40 -0500

    Add files via upload

--- src/WeightSort.java
+++ src/WeightSort.java
@@ -1,29 +1,22 @@
 import java.util.*;
 
 public class WeightSort {
-    public static void main(String[] args) {
-        WeightSort obj = new WeightSort();
-        String[] strs = {"CAT", "DOG", "BAT", "ANT"};
-        String[] ret = obj.sort(strs);
-        System.out.println(Arrays.toString(ret));
-    }
-
     public String[] sort(String[] strs) {
+        
         Arrays.sort(strs);
-        
-        Comparator<String> comp = (a,b) -> weight(a) - weight(b);
-        Arrays.sort(strs, comp);
-        
+
+        Arrays.sort(strs, (a,b) -> weight(a) - weight(b));
+
         return strs;
     }
 
     private int weight(String s){
-        int ret = 0;
+        int sum = 0;
 
-        for(Character ch: s.toCharArray()){
-            int w = ch - 'A' + 1;
-            ret += w;
+        for(Character c: s.toCharArray()){
+            sum += c - 'A' + 1;
         }
-        return ret;
+
+        return sum;
     }
 }
